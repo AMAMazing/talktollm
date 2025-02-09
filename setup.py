edf5b5a501c6a864e15a7befd7ce932e2b1bd60c
AMA Mazing <alexmalone489@gmail.com>
2025-02-09 13:18:20 +0700

    Dev update: readme and setup

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from setuptools import setup, find_packages
 
 setup(
     name='talktollm',
-    version='0.1.0',
+    version='0.1.1',
     packages=find_packages(),
     include_package_data=True,
     package_data={
-        'talktollm': ['images/*/*'],
+        'talktollm': ['images//*'],
     },
     install_requires=[
         'pywin32',
@@ -19,4 +19,15 @@ setup(
             'talktollm=talktollm:talkto',
         ],
     },
+    author="Alex M",
+    description="A Python utility for interacting with large language models (LLMs) through a command-line interface",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/AMAMazing/talktollm",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
 )
